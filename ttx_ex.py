5d1b10ac88ad67f9372ea3937823cf35e2865c66
iforsmann <iforsmann@frontier.com>
2023-03-12 02:05:34 +0000

    it works.... mwahahahahha

--- ttx_ex.py
+++ ttx_ex.py
@@ -1,10 +1,24 @@
-from aiy.voice import tts
-
+from aiy.voice.tts import *
+from aiy.cloudspeech import *
+from aiy.board import Board
+from aiy.leds import Leds, Color
 
 def main():
-	
+    client = CloudSpeechClient()
+    with Board() as board, Leds() as leds:
+        while True:
+            print("press to begin recording...")
+            board.button.wait_for_press()
+            leds.update(Leds.rgb_on(Color.PURPLE))
+            text = client.recognize()
+            print("press again for reply...")
+            board.button.wait_for_press()
+            leds.update(Leds.rgb_off())
+            say(text)
+
+
 
 if __name__ == "__main__":
-	main()
+    main()
 
 
